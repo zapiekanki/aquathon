80519ac9856d2a1a394f3c39d33267c2b107f5a9
dominik.kozlowski <dominik.kozlowski@vonroll-infratec.world>
2022-06-11 15:08:00 +0200

    added map

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +7,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'aquathon';
+  @ViewChild('mapContainer', { static: false }) gmap?: ElementRef;
+  map?: google.maps.Map;
+  lat = 50.041187;
+  lng = 21.999121;
 
+  coordinates = new google.maps.LatLng(this.lat, this.lng);
+
+  mapOptions: google.maps.MapOptions = {
+    center: this.coordinates,
+    zoom: 12
+  };
+
+  ngAfterViewInit() {
+    this.mapInitializer();
+  }
+
+  mapInitializer() {
+    this.map = new google.maps.Map(this.gmap?.nativeElement,
+      this.mapOptions);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,30 @@
-import {APP_INITIALIZER, NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import {AppComponent} from './app.component';
-import {FIREBASE} from "./firebase/firebase.initializer";
+import { AppComponent } from './app.component';
+import { FIREBASE } from "./firebase/firebase.initializer";
+import { GoogleMapsModule } from "@angular/google-maps";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatIconModule } from "@angular/material/icon";
+import { MatButtonModule } from "@angular/material/button";
+import { HttpClientJsonpModule, HttpClientModule } from "@angular/common/http";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    GoogleMapsModule,
+    MatSidenavModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    HttpClientModule,
+    HttpClientJsonpModule
   ],
   providers: [
     {
